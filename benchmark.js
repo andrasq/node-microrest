ed9d4af5a6cf522c75a1f4b3b1d66ba71ca452f2
Andras <andras@kinvey.com>
2018-04-29 13:10:20 -0400

    time more frameworks (not all working yet)

--- benchmark.js
+++ benchmark.js
@@ -1,4 +1,4 @@
-// npm install microreq qtimeit restify express qrpc
+// npm install microreq qtimeit restify express qrpc connect restiq
 // wrk -d4s -t2 -c8 'http://localhost:1337/echo?a=1&b=2&c=3
 
 
@@ -12,7 +12,10 @@ var qtimeit = require('qtimeit');
 var frameworks = {
     restify: { pkg: require('restify'), ver: require('restify/package').version, port: 1337 },
     express: { pkg: require('express'), ver: require('express/package').version, port: 1338 },
-    rest_mw: { pkg: require('./'), ver: require('./package').version, port: 1342 },
+    restiq: { pkg: require('restiq'), ver: require('restiq/package').version, port: 1345 },
+//    connect: { pkg: require('connect'), ver: require('connect/package').version, port: 1346 },
+//    rest_mw: { pkg: require('./'), ver: require('./package').version, port: 1342 },
+    rest_ha: { pkg: require('./'), ver: require('./package').version, port: 1347 },
     rest: { pkg: require('./'), ver: require('./package').version, port: 1339 },
     http_buf: { pkg: require('http'), ver: process.version, port: 1344 },
     http: { pkg: require('http'), ver: process.version, port: 1340 },
@@ -50,31 +53,54 @@ if (cluster.isMaster) {
         // 23.6k/s
     }
 
+    if (frameworks.restiq) {
+        // 13.8k/s 259us
+        servers.restiq = frameworks.restiq.pkg.createServer({ restify: true });
+        servers.restiq.listen(frameworks.restiq.port);
+        servers.restiq.get(path1, function(req, res, next) { res.send(200, response1); next(); })
+    }
+
+    if (frameworks.connect) {
+        servers.connect = frameworks.connect.pkg();
+        servers.connect.use(path1, function(req, res, next) { res.end(response1); next(); })
+        http.createServer(servers.connect).listen(servers.connect.port);
+    }
+
     if (frameworks.rest_mw) {
         // 44.5k/s 86.6us
         servers.rest_mw = frameworks.rest.pkg.createServer({ port: frameworks.rest_mw.port });
         servers.rest_mw._rest.router = new (require('./router'))();
-        //servers.rest_mw._rest.addRoute('/test1', function(req, res, next) { servers.rest._rest.sendResponse(req, res, noop, null, 200, response1); });
+        //servers.rest_mw._rest.setRoute('/test1', function(req, res, next) { servers.rest._rest.sendResponse(req, res, noop, null, 200, response1); });
         // 41k/s
-        servers.rest_mw._rest.addRoute('/test1', function test1(req, res, next) { res.end(response1); });
+        servers.rest_mw._rest.setRoute('/test1', function test1(req, res, next) { res.end(response1); });
         // 42.6k/s
         function noop(){}
+    }
+
+    if (frameworks.rest_ha) {
+        servers.rest_ha = frameworks.rest.pkg({ processRequest: processRequest });
+        http.createServer(servers.rest_ha).listen(frameworks.rest_ha.port);
+        function noop(){}
+        function processRequest(req, res) {
+            if (req.url === path1 && req.method === 'GET') {
+                return servers.rest_ha.rest.sendResponse(req, res, noop, null, 200, response1);
+            }
+            servers.rest._rest.sendResponse(req, res, noop, new servers.rest._rest.HttpError(404, 'path not routed'));
+        }
     }
 
     if (frameworks.rest) {
         // 48.8k 77.2us
         servers.rest = frameworks.rest.pkg.createServer({ port: frameworks.rest.port });
+        function noop(){}
         servers.rest._rest.processRequest = function(req, res) {
             if (req.url === path1 && req.method === 'GET') {
                 //res.end(response1);
                 // 31k/s
-                servers.rest._rest.sendResponse(req, res, noop, null, 200, response1);
+                return servers.rest._rest.sendResponse(req, res, noop, null, 200, response1);
                 // 32k/s, 128us stddev 24.1us
             }
-            else {
-                servers.rest._rest.sendResponse(req, res, noop, new servers.rest._rest.HttpError(404, 'path not routed'));
-            }
-            function noop(){}
+            servers.rest._rest.sendResponse(req, res, noop, new servers.rest._rest.HttpError(404, 'path not routed'));
         }
     }
 
@@ -140,7 +166,7 @@ else {
 
     var parallelTests = {};
     var agents = {};    // reuse all agents, else qrpc consumes all sockets
-    var verifyResponse = false;
+    var verifyResponse = true;
     var parallelCallCount = 100;
 
     setTimeout(setupTests, 100);
@@ -170,10 +196,11 @@ else {
             path: path1,
         };
 
+        var testBody = new Array(201).join('x');
         var responseIndex = name === 'qrpc' ? 1 : 2;
         var makeCall = name === 'qrpc'
             ? function(cb) { uri.agent.call(uri.path, null, cb) }
-            : function(cb) { microreq(uri, '', cb) }
+            : function(cb) { microreq(uri, testBody, cb) }
 
         function makeQrpcCb(cb) {
             return function(err, ret) { cb(err, {}, ret) }
@@ -183,6 +210,7 @@ else {
             var ncalls = callCount, ndone = 0;
             for (var i=0; i<ncalls; i++) makeCall(onBack);
             function onBack(err, res, body) {
+                if (err) { console.log("AR: http err", err); process.exit(); }
                 if (verifyResponse && String(arguments[responseIndex]) != response1 && JSON.parse(arguments[responseIndex]) != response1) {
                     console.log("AR: wrong response:", err, String(arguments[responseIndex]));
                     throw new Error("wrong response")
@@ -198,41 +226,29 @@ else {
         console.log("AR: runSuite");
 
         // NOTE: to disable, set -d to 0 sec
-        var cmdline = 'wrk -d2s -t2 -c4 http://localhost:%d/test1';
-        console.log("AR: restify:");
-        child_process.exec(util.format(cmdline, frameworks.restify.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
-        console.log("AR: express:");
-        child_process.exec(util.format(cmdline, frameworks.express.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
-        console.log("AR: rest_mw:");
-        child_process.exec(util.format(cmdline, frameworks.rest_mw.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
-        console.log("AR: rest:");
-        child_process.exec(util.format(cmdline, frameworks.rest.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
-        console.log("AR: http_buf:");
-        child_process.exec(util.format(cmdline, frameworks.http_buf.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
-        console.log("AR: http:");
-        child_process.exec(util.format(cmdline, frameworks.http.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
-        console.log("AR: http_raw:");
-        child_process.exec(util.format(cmdline, frameworks.http_raw.port), function(err, stdout, stderr) {
-            if (!err) console.log(stdout + '\n');
+        var cmdline = 'wrk -d0s -t2 -c4 http://localhost:%d/test1';
 
-        qtimeit.bench.timeGoal = .2;
-        qtimeit.bench.visualize = true;
-        qtimeit.bench.showRunDetails = false;
+        for (var name in frameworks) {
+            var cmd = util.format(cmdline, frameworks[name].port);
+            console.log("AR: %s %s", name, cmd);
+            if (name !== 'qrpc') child_process.exec(cmd, function(err, stdout, stderr) {
+            })
+        }
+        setTimeout(function() {
 
-        console.log("AR: bursts of %d calls", parallelCallCount);
-        if (1)
-        qtimeit.bench(parallelTests, function() {
-        qtimeit.bench(parallelTests, function() {
-        qtimeit.bench(parallelTests, function() {
+            qtimeit.bench.timeGoal = .2;
+            qtimeit.bench.visualize = true;
+            qtimeit.bench.showRunDetails = false;
+
+            console.log("AR: bursts of %d calls", parallelCallCount);
+            if (1)
+            qtimeit.bench(parallelTests, function() {
+            qtimeit.bench(parallelTests, function() {
+            qtimeit.bench(parallelTests, function() {
 
 
             console.log("AR: Done.");
-        }) }) }) }) }) }) }) }) }) })
+            }) }) })
+        }, 200);
     }
 }
